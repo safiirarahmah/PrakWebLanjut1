6abbd166a96c781e043aed3b543dc0bd16b832a4
safiirarahmahlinisa-2017051015 <safiira.rahmahlinisa2015@students.unila.ac.id>
2022-09-14 03:54:11 +0700

    praktikum 3

--- prakWebLanjutB/app/Controllers/Pages.php
+++ prakWebLanjutB/app/Controllers/Pages.php
@@ -3,17 +3,17 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\MahasiswaModel;
 
 class Pages extends BaseController
 {
     public function index()
     {
-        return view('index');
+        return view('welcome_message');
     }
-
     public function view($page = 'home')
     {
-        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+        if (!is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
             // Whoops, we don't have a page for that!
             throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
         }
@@ -23,5 +23,35 @@ class Pages extends BaseController
         return view('templates/header', $data)
             . view('pages/' . $page)
             . view('templates/footer');
+    }
+    public function home()
+    {
+        $data['title'] = 'Home';
+        return view('templates/header', $data)
+            . view('pages/home')
+            . view('templates/footer');
+    }
+    public function about()
+    {
+        $data['title'] = 'About';
+        return view('templates/header', $data)
+            . view('pages/about')
+            . view('templates/footer');
+    }
+    public function mahasiswa()
+    {
+        $mahasiswaModel = new MahasiswaModel();
+        $mahasiswa = $mahasiswaModel->findAll();
+        $data = [
+            'title' => 'Mahasiswa',
+            'mahasiswa' => $mahasiswa
+        ];
+        return view('templates/header', $data)
+            . view('pages/mahasiswa', $data)
+            . view('templates/footer');
+    }
+    public function test()
+    {
+        dd('test');
     }
 }
--- prakWebLanjutB/app/Views/templates/header.php
+++ prakWebLanjutB/app/Views/templates/header.php
@@ -1,32 +1,35 @@
 <!doctype html>
 <html>
+
 <head>
-    <title>CodeIgniter Tutorial</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>CodeIgniter Tutorial</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Web Lanjut</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/mahasiswa">Mahasiswa</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Web Lanjut</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/mahasiswa">Mahasiswa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-    <h1><?= esc($title) ?></h1>
+  </nav>
+  <h1 class="mt-3 ml-4"><?= esc($title) ?></h1>
+  <div class="p-4"></div>
